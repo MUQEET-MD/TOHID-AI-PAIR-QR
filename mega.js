52d97dfcd8b468884aebc6db92d27502deec44b0
MUQEET-MD <quotexmarketview0@gmail.com>
2025-06-03 10:55:04 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,7 +2,7 @@ const { Storage } = require("megajs");
 
 const auth = {
     email: 'muqueetyt@gmail.com',
-    password: 'Usatiktok1$',
+    password: 'Usatiktok1',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
